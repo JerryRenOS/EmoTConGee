6d329d5bd9982f493c5f3684ccfffc1b4f573966
Jerry Ren <yren@gm.slc.edu>
2020-06-17 22:52:14 -0400

    EmoTConGee initiation

---
+++ EmoTConGee/EmoTConGee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmoTConGee
+//
+//  Created by Jerry Ren on 6/17/20.
+//  Copyright © 2020 Jerry Ren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
