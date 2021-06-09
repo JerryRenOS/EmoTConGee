c93a04572f3d7bf8fb42f781d292158ea9b2bea8
jer .explosion <jerryren.ios@gmail.com>
2021-06-08 23:59:38 -0400

    Revert "Revert "er * Greened up""""
    This reverts commit 84c419d6ba98632944d0f4b61cdd6f1fc48bdab4.

---
+++ EmoTConGee/EmoticonSentimentalMaker.playground/Contents.swift
@@ -0,0 +1,35 @@
+import Cocoa
+import CreateML
+
+let data = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/jerryren/Desktop/twitter-sanders-apple3.csv"))
+
+let(trainingData, testingData) = data.randomSplit(by: 0.80, seed: 5)
+// randomly taking 80 percent of the data to assign to training, the rest for testing
+
+let sentimentClassifier = try MLTextClassifier(trainingData: trainingData, textColumn: "text", labelColumn: "class")
+
+let evaluationMetrics = sentimentClassifier.evaluation(on: testingData)
+
+let evaluationAccuracy = (1.0 - evaluationMetrics.classificationError) * 100
+
+let metadata = MLModelMetadata(author: "Jerry Ren", shortDescription: "A model trained to classify Tweets by emotional valence",version: "1.0")
+
+try sentimentClassifier.write(to: URL(fileURLWithPath:"/Users/jerryren/Desktop/EmotiConjiTweetoClassifier.mlmodel" ))
+
+
+// test 'em out below //
+
+try sentimentClassifier.prediction(from: "I love Pokemon!")
+
+try sentimentClassifier.prediction(from: "Best Sausage Egg and Cheese I've had this year!")
+
+try sentimentClassifier.prediction(from: "Bates tastes ok")
+
+try sentimentClassifier.prediction(from: "I hate pizza!")
+
+try sentimentClassifier.prediction(from: "@Netflix is an average company~")
+
+try sentimentClassifier.prediction(from: "Justin Bieber sucks!")
+
+try sentimentClassifier.prediction(from: "Sunsets are beautiful!")
+ 
